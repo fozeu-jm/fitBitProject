ca67543cd871483f86910ea9170983f850054050
fozeu-jm <fozeu.jmgmail.com>
2019-11-20 05:25:06 +0100

    custom table view controller ongoing

---
+++ fitBit/sessionCell.swift
@@ -0,0 +1,24 @@
+//
+//  sessionCell.swift
+//  fitBit
+//
+//  Created by KAIZER WEB DESIGN on 20/11/2019.
+//  Copyright © 2019 kaizer. All rights reserved.
+//
+
+import UIKit
+
+class sessionCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLab: UILabel!
+    @IBOutlet weak var distanceLab: UILabel!
+    
+    @IBOutlet weak var hourLab: UILabel!
+    @IBOutlet weak var minsLab: UILabel!
+    @IBOutlet weak var secLab: UILabel!
+    @IBOutlet weak var sourLat: UILabel!
+    @IBOutlet weak var sourLong: UILabel!
+    @IBOutlet weak var destLat: UILabel!
+    @IBOutlet weak var destLong: UILabel!
+    
+}
---
+++ fitBit/workoutSession.swift
@@ -0,0 +1,34 @@
+//
+//  workoutSession.swift
+//  fitBit
+//
+//  Created by KAIZER WEB DESIGN on 20/11/2019.
+//  Copyright © 2019 kaizer. All rights reserved.
+//
+
+import Foundation
+
+class workoutSession {
+    var startTime: String
+    var endTime: String
+    var duration: Int
+    var distance: Double
+    var sourLat: Double
+    var sourLong: Double
+    var destLat: Double
+    var destLong: Double
+    
+    init(startTime: String,endTime: String, duration: Int, distance: Double, sourLat: Double, sourLong: Double, destLat: Double, destLong: Double) {
+        
+        self.startTime = startTime
+        self.endTime = endTime
+        self.duration = duration
+        self.distance = distance
+        self.sourLat = sourLat
+        self.sourLong = sourLong
+        self.destLat = destLat
+        self.destLong = destLong
+        
+    }
+    
+}
