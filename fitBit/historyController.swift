6a29abe8383e7ee57489f9dc740ac1552f35ec15
fozeu-jm <fozeu.jmgmail.com>
2019-11-20 05:49:35 +0100

    table view controller updated

--- fitBit/historyController.swift
+++ fitBit/historyController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import SQLite3
 
 class historyController: UITableViewController {
-
+    
+    
+    var db : OpaquePointer?
+    var sessions: [workoutSession] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadDB()
+        sessions = createArray()
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    
+    func loadDB(){
+        let fileUrl = try!
+            FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("fitbit.sqlite")
+        
+        if sqlite3_open(fileUrl.path, &db) != SQLITE_OK{
+            print("Error opening file")
+        }
+        
+    }
+    
+    func createArray() -> [workoutSession] {
+        var tempSessions: [workoutSession] = []
+        
+        var stmt2 : OpaquePointer?
+        let select = "SELECT * FROM fitBit"
+        
+        if sqlite3_prepare(db, select, -1, &stmt2, nil) == SQLITE_OK {
+            while sqlite3_step(stmt2) == SQLITE_ROW {
+                
+                let date = UnsafePointer<UInt8>(sqlite3_column_text(stmt2, 1))!
+                let begin = String(cString: date)
+                
+                let date2 = UnsafePointer<UInt8>(sqlite3_column_text(stmt2, 2))!
+                let end = String(cString: date2)
+                
+                let duration = Int(sqlite3_column_int(stmt2, 3))
+                let distance = sqlite3_column_double(stmt2, 4)
+                let sourlat = sqlite3_column_double(stmt2, 5)
+                let sourlong = sqlite3_column_double(stmt2, 6)
+                let destlat = sqlite3_column_double(stmt2, 7)
+                let destlong = sqlite3_column_double(stmt2, 8)
+                
+                let session = workoutSession(startTime: begin, endTime: end, duration: duration, distance: distance, sourLat: sourlat, sourLong: sourlong, destLat: destlat, destLong: destlong)
+                tempSessions.append(session)
+            }
+        }else{
+            print(String.init(cString: sqlite3_errmsg(db)))
+        }
+        
+        return tempSessions
+    }
+    
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -31,14 +83,6 @@ class historyController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
-    
-    
-    
-    func createArray() -> [workoutSession] {
-        let tempSessions: [workoutSession] = []
-        return tempSessions
-    }
-
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
